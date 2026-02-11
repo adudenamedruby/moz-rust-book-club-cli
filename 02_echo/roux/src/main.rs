fedacd9526a06072feccd392ef1b5e239d136995
roux g. buciu <github@adudenamedruby.com>
2026-02-11 11:45:06 -0500

    roux - finish ch2

--- 02_echo/roux/src/main.rs
+++ 02_echo/roux/src/main.rs
@@ -1,44 +1,23 @@
-//use std::env::args;
-use clap::{Arg, ArgAction, Command};
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `echo`
+struct Args {
+    /// Input text
+    #[arg(required(true))]
+    text: Vec<String>,
+
+    /// Do not print newline
+    #[arg(short('n'))]
+    omit_newline: bool,
+}
 
 fn main() {
-    // println!("{:?}", args());
-    let matches = Command::new("echor") // create new instance of App
-        .version("0.1.0") // use semantic versioning
-        .author("Roux Buciu") // include author info
-        .about("Rust version of echo") // a short description of the program
-        .arg(
-            Arg::new("text")
-                .value_name("TEXT")
-                .help("Input text")
-                .required(true)
-                .num_args(1..),
-        )
-        .arg(
-            Arg::new("omit_newline")
-                .short('n')
-                .action(ArgAction::SetTrue)
-                .help("Do not print newline"),
-        )
-        .get_matches(); // parse the arguments
-
-    // must specify the type of vec, because iterator can be of a bunch of types
-    let text: Vec<String> = matches.get_many("text").unwrap().cloned().collect();
-    // rust infers where it can
-    let omit_newline = matches.get_flag("omit_newline");
-
-    // works
-    // let mut ending = "\n";
-    // if omit_newline {
-    //     ending = "";
-    // }
-
-    // more Rust like
-    // let ending = if omit_newline { "" } else { "\n" };
-
-    // print!("{}{}", text.join(" "), ending);
-
-    // more functional
-    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
-    // println!("{:#?}", matches)
+    let args = Args::parse();
+    print!(
+        "{}{}",
+        args.text.join(" "),
+        if args.omit_newline { "" } else { "\n" }
+    );
 }
--- 02_echo/roux/tests/cli.rs
+++ 02_echo/roux/tests/cli.rs
@@ -1,16 +1,55 @@
+use anyhow::Result;
 use assert_cmd::{Command, cargo};
 use predicates::prelude::*;
+use pretty_assertions::assert_eq;
+use std::fs;
+
+const TEST_DIR: &str = "tests/expected/";
 
 #[test]
-fn dies_no_args() {
+fn dies_no_args() -> Result<()> {
     let mut cmd = Command::new(cargo::cargo_bin!("echor"));
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("Usage"));
+    Ok(())
 }
 
 #[test]
 fn runs() {
     let mut cmd = Command::new(cargo::cargo_bin!("echor"));
     cmd.arg("hello").assert().success();
+}
+
+#[test]
+fn hello1() -> Result<()> {
+    run_file_test(&["Hello there"], "hello1.txt")
+}
+
+#[test]
+fn hello2() -> Result<()> {
+    run_file_test(&["Hello", "there"], "hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> Result<()> {
+    run_file_test(&["Hello  there", "-n"], "hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> Result<()> {
+    run_file_test(&["-n", "Hello", "there"], "hello2.n.txt")
+}
+
+fn run_file_test(args: &[&str], expected_file: &str) -> Result<()> {
+    let expected = fs::read_to_string(format!("{}{}", TEST_DIR, expected_file))?;
+    let output = Command::new(cargo::cargo_bin!("echor"))
+        .args(args)
+        .output()
+        .expect("fail");
+
+    let stout = String::from_utf8(output.stdout).expect("invalid UTF-8");
+    assert_eq!(stout, expected);
+
+    Ok(())
 }
