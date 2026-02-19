8834eafceb444b666a19247af2d8222ca6dafcdd
Temisan Iwere <tiwere@mozilla.com>
2026-02-19 18:52:02 -0500

    temisan - finish ch2

---
+++ 02_echo/temisan/src/main.rs
@@ -0,0 +1,24 @@
+use clap::Parser;
+// use clap::{Arg, ArgAction, Command};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `echo`
+struct Args {
+    /// Input text
+    #[arg(required(true))]
+    text: Vec<String>,
+
+    /// Do not print newline
+    #[arg(short('n'))]
+    omit_newline: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+    print!(
+        "{}{}",
+        args.text.join(" "),
+        if args.omit_newline { "" } else { "\n" }
+    );
+}
