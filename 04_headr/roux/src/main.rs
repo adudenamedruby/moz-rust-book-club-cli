bb9725170d4730488411aaeb048cfe85bbb588e5
roux g. buciu <github@adudenamedruby.com>
2026-04-13 17:15:45 -0400

    trying out the derive pattern

--- 04_headr/roux/src/main.rs
+++ 04_headr/roux/src/main.rs
@@ -1,1 +1,28 @@
-fn main() {}
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(version, author, about, long_about=None)]
+struct Args {
+    /// Input file(s)
+    #[arg(value_name = "FILES", default_value = "-")]
+    file: Vec<String>,
+
+    /// Number of lines
+    #[arg(
+        short = 'n',
+        long,
+        default_value = "10",
+        conflicts_with = "bytes",
+        value_parser
+    )]
+    lines: u64,
+
+    /// Number of bytes
+    #[arg(short = 'c', long, value_parser)]
+    bytes: Option<u64>,
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("{:#?}", args)
+}
