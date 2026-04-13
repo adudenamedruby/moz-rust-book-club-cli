707ac9ab8351efac12283883ba0f04346b35a6df
roux g. buciu <github@adudenamedruby.com>
2026-04-13 18:45:33 -0400

    working on ch 4

--- 04_headr/roux/src/main.rs
+++ 04_headr/roux/src/main.rs
@@ -1,11 +1,17 @@
+use std::{
+    fs::{File, metadata},
+    io::{self, BufRead, BufReader},
+};
+
+use anyhow::Result;
 use clap::Parser;
 
 #[derive(Debug, Parser)]
 #[command(version, author, about, long_about=None)]
 struct Args {
     /// Input file(s)
-    #[arg(value_name = "FILES", default_value = "-")]
-    file: Vec<String>,
+    #[arg(value_name = "FILE", default_value = "-")]
+    files: Vec<String>,
 
     /// Number of lines
     #[arg(
@@ -13,16 +19,58 @@ struct Args {
         long,
         default_value = "10",
         conflicts_with = "bytes",
-        value_parser
+        value_parser = clap::value_parser!(u64).range(1..)
     )]
     lines: u64,
 
     /// Number of bytes
-    #[arg(short = 'c', long, value_parser)]
+    #[arg(short = 'c', long, value_parser = clap::value_parser!(u64).range(1..))]
     bytes: Option<u64>,
 }
 
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+fn run(args: Args) -> Result<()> {
+    for filename in args.files {
+        match open(&filename) {
+            Err(e) => eprintln!("{filename}: {e}\n"),
+            Ok(mut file) => match metadata(&filename) {
+                Err(e) => eprintln!("{filename}: {e}"),
+                Ok(metadata) => {
+                    if metadata.len() != 0 {
+                        println!("{}", String::from("==> {filename} <=="));
+
+                        if let Some(byte_num) = args.bytes {
+                            let mut buf = vec![0; byte_num as usize];
+                            file.read_exact(&mut buf)?;
+                            let byte_str = String::from_utf8_lossy(&buf);
+                            print!("{}", byte_str);
+                        } else {
+                            for line in file.lines().take(args.lines as usize) {
+                                match line {
+                                    Err(e) => eprintln!("no line present: {e}"),
+                                    Ok(line) => {
+                                        print!("{line}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+        }
+    }
+    Ok(())
+}
+
 fn main() {
-    let args = Args::parse();
-    println!("{:#?}", args)
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1)
+    }
 }
