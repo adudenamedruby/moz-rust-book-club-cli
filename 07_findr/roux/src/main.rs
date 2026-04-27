143e883e6b1b5c17b65dd7118caaeea6fd91a20e
roux g. buciu <github@adudenamedruby.com>
2026-04-27 17:30:21 -0400

    building arguments

--- 07_findr/roux/src/main.rs
+++ 07_findr/roux/src/main.rs
@@ -1,3 +1,56 @@
+use clap::{ArgAction, Parser, ValueEnum, builder::PossibleValue};
+use regex::Regex;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+enum EntryType {
+    Dir,
+    File,
+    Link,
+}
+
+impl ValueEnum for EntryType {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[EntryType::Dir, EntryType::File, EntryType::Link]
+    }
+    fn to_possible_value(&self) -> Option<clap::builder::PossibleValue> {
+        Some(match self {
+            EntryType::Dir => PossibleValue::new("d"),
+            EntryType::File => PossibleValue::new("f"),
+            EntryType::Link => PossibleValue::new("l"),
+        })
+    }
+}
+
+#[derive(Debug, Parser)]
+#[command(author = "adudenamedruby", version, about)]
+struct Args {
+    /// Search paths
+    #[arg(value_name = "PATH", default_value = ".")]
+    paths: Vec<String>,
+
+    /// Name
+    #[arg(short('n'),
+        long("name"),
+        value_name = "NAME",
+        value_parser(Regex::new),
+        action(ArgAction::Append),
+        num_args(0..)
+        )]
+    name: Vec<Regex>,
+
+    /// Entry type
+    #[arg(
+        short('t'),
+        long("type"),
+        value_name = "TYPE",
+        value_parser(clap::value_parser!(EntryType)),
+        action(ArgAction::Append),
+        num_args(0..)
+    )]
+    entry_types: Vec<EntryType>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{args:#?}")
 }
